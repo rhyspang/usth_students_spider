84ede3a07a9c291d722c097a847e3a3e34a8c294
rhyspang <rhyspang@gmail.com>
2017-07-07 17:41:17 +0800

    ignore scrapyd dbs

---
+++ students/mail/mail.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17-7-7 下午1:47
+# @Author  : rhys
+# @Software: PyCharm
+# @Project : students
---
+++ students/signals.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17-7-7 下午1:37
+# @Author  : rhys
+# @Software: PyCharm
+# @Project : students
